b94af069610a59d92ef8346171f2d112fa29195d
AMI01012026 <andreja.mijadzikovic@alsglobal.com>
2026-07-06 11:59:30 +0200

    Default Supabase configuration

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.OMW_DEFAULT_CONFIG = {
-  supabaseUrl: "",
-  supabaseKey: ""
+  supabaseUrl: "https://krlcddtzyvrukjfngzbq.supabase.co",
+  supabaseKey: "sb_publishable_9GwvfYtkDKh28I_FsUs56A_oH9UFOzm"
 };
